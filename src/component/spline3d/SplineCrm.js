02c6d72f6f64d2cdc13842cdd68de3fd65712774
mohd zaid <mohd@skilliza.com>
2022-07-28 18:21:57 +0530

    initial commit

---
+++ src/component/spline3d/SplineCrm.js
@@ -0,0 +1,18 @@
+import Spline from '@splinetool/react-spline'
+import React from 'react'
+import './SplineCrm.css'
+
+
+const SplineCrm = () => {
+  return (
+  <div>
+    <div className='spline-wrapper'>
+      <Spline className='splinecrm-3d' scene="https://prod.spline.design/WmQfNboykBYXMTfh/scene.splinecode" />
+    </div>
+   
+  </div>
+
+)
+}
+
+export default SplineCrm
